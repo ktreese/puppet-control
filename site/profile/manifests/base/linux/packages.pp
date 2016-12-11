c2f8c2a1cd33f99531ad488238e1bb59606ecb2e
Kris Reese <kris.reese@wwt.com>
2016-12-10 22:56:19 -0600

    Corrects package name for chrony

--- site/profile/manifests/base/linux/packages.pp
+++ site/profile/manifests/base/linux/packages.pp
@@ -2,7 +2,7 @@ class profile::base::linux::packages {
 
   $timekeeper = $::facts['os']['release']['major'] ? {
     '6'     => 'ntpd',
-    '7'     => 'chronyd',
+    '7'     => 'chrony',
     default => 'ntpd',
   }
 
