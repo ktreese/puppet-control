147e7c057e68c119f4733947bd5a8d4f4cd26e28
Kris Reese <kris.reese@wwt.com>
2016-12-13 08:55:00 -0600

    Moves host resources into profile

--- site/profile/manifests/dnsmasq.pp
+++ site/profile/manifests/dnsmasq.pp
@@ -1,3 +1,20 @@
 class profile::dnsmasq {
   include dnsmasq
+
+  # Add hosts that cannot have puppet agent
+  host { 'ts140.3031.net':
+    ensure       => present,
+    host_aliases => ['ts140'],
+    ip           => '192.168.1.2',
+    notify       => Service['dnsmasq'],
+    require      => Package['dnsmasq'],
+  }
+
+  host { 'qnreese01.3031.net':
+    ensure       => present,
+    host_aliases => ['qnreese01'],
+    ip           => '192.168.1.5',
+    notify       => Service['dnsmasq'],
+    require      => Package['dnsmasq'],
+  }
 }
