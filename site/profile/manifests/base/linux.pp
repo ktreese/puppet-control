d354e3e4c6aa1e331d8e18e98c3f0436b47e8daf
Kris Reese <kris.reese@wwt.com>
2017-01-25 20:46:01 -0600

    Includes environment in base profile

--- site/profile/manifests/base/linux.pp
+++ site/profile/manifests/base/linux.pp
@@ -2,4 +2,5 @@ class profile::base::linux {
   include hosts
   include 'profile::base::linux::packages'
   include 'profile::base::linux::services'
+  include 'profile::base::linux::environment'
 }
